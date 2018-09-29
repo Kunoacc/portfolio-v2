0f307257b335d976b504967747c06ecf654c4ef0
Nelly Atuonwu <nellyatuonwu@gmail.com>
2018-09-29 11:08:35 +0100

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@
 </template>
 
 <style lang="scss">
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -26,4 +27,5 @@
     }
   }
 }
+
 </style>
